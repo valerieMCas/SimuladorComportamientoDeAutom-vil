950d983f7724e96dcb4fb7e8dc5c871a163fb404
Camila <mariac.pradac@autonoma.edu.co>
2025-04-03 19:14:34 -0500

    Se crearon los paquetes

--- src/autonoma/simulador/main/SimuladorAutomovilApp.java
+++ src/autonoma/simulador/main/SimuladorAutomovilApp.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package simuladorcomportacientoautomovil;
+package autonoma.simulador.main;
 
 /**
  *
  * @author usuario
  */
-public class SimuladorComportacientoAutomovil {
+public class SimuladorAutomovilApp {
 
     /**
      * @param args the command line arguments
